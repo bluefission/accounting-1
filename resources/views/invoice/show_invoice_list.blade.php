2a3e6f2e036b6f3f81cc5112355d068466d9b725
Liam Demafelix <liamdemafelix.n@gmail.com>
2017-02-05 20:36:10 +0800

    Whoops.

--- resources/views/invoice/show_invoice_list.blade.php
+++ resources/views/invoice/show_invoice_list.blade.php
@@ -31,7 +31,7 @@
                     		<tbody>
                     			@foreach($invoiceList as $invoice)
                     				<tr>
-	                            		<!-- <td><a href="{{route('invoice.show',$invoice->id)}}">{{sprintf("%'.07d\n", $invoice->id)}}</a></td> -->+
+	                            		<!-- <td><a href="{{route('invoice.show',$invoice->id)}}">{{sprintf("%'.07d\n", $invoice->id)}}</a></td> -->
 											<td><a href="{{route('invoice.show',$invoice->id)}}">INV-{{strtoupper(substr(md5($invoice->id), 0, 5))}}</a></td>
 	                            		<td>
 	                            			@if($invoice->is_paid)
