d17bf5e543de9b8bea669e2dc0bf9a99ad2dd3e9
Liam Demafelix <liamdemafelix.n@gmail.com>
2017-02-05 23:28:37 +0800

    Fix journal

--- app/Http/Controllers/Utility/UtilityHelper.php
+++ app/Http/Controllers/Utility/UtilityHelper.php
@@ -463,7 +463,7 @@ trait UtilityHelper
                                         ->get();
         }elseif($modelName==='JournalModel'){
             $value = Auth::user()->branch_id;
-            $query = JournalModel::whereYear('created_at','=',date('Y'));
+            $query = JournalModel::all();
             if(!(is_null($value)) && !(Auth::user()->branchInfo->main_office)){
                 $query->whereHas('userCreateInfo',function($q) use ($value){
                             $q->where('branch_id','=',$value);
